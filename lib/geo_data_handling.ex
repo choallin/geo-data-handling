b8df34f82863e370bb9915ef6e39c06988e6fd6c
gernot <gernot.gradwohl@gruberreisen.at>
2018-09-06 15:55:40 +0200

    NEW: Pair matching done

--- lib/geo_data_handling.ex
+++ lib/geo_data_handling.ex
@@ -3,16 +3,13 @@ defmodule GeoDataHandling do
   Documentation for GeoDataHandling.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GeoDataHandling.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def create_pairs(file_path) do
+    file_path
+    |> Path.expand(__DIR__)
+    |> File.stream!
+    |> CSV.decode!(headers: true)
+    |> Enum.map(&( [ String.to_float(&1["lon"]) , String.to_float(&1["lat"]) ] ))
+    |> Enum.take(2)
+    |> Enum.map(&(&1))
   end
 end
--- test/geo_data_handling_test.exs
+++ test/geo_data_handling_test.exs
@@ -2,7 +2,9 @@ defmodule GeoDataHandlingTest do
   use ExUnit.Case
   doctest GeoDataHandling
 
-  test "greets the world" do
-    assert GeoDataHandling.hello() == :world
+  test "creates pairs" do
+    assert Enum.take(GeoDataHandling.create_pairs("../pairs.csv"), 2) == [
+      [120.99287, 14.75659], [120.99206, 14.756699999999999]
+    ]
   end
 end
